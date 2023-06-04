d87ce7f038076409c584317e38819d5df427a479
abooda sayed <savvyhost25@gmail.com>
2023-06-05 01:21:39 +0200

    first commit

---
+++ resources/views/custom/header.blade.php
@@ -0,0 +1,20 @@
+<header class="base-header">
+
+    <div class="header-pagination"></div>
+
+    <div class="header-swiper">
+        <div class="swiper-wrapper">
+            <div class="swiper-slide"><img src="static/image/header/3.jpg">
+                <a href="#" class="banner-text ">Do more with MamnonTravel</a>
+
+            </div>
+            <div class="swiper-slide"><img src="static/image/header/1.jpg">
+                <a href="#" class="banner-text">Read about Pyramids</a>
+            </div>
+            <div class="swiper-slide"><img src="static/image/header/2.jpg">
+                <a href="#" class="banner-text ">Trip inspo for every kind of traveler</a>
+            </div>
+        </div>
+    </div>
+    @include('custom.search')
+</header>
